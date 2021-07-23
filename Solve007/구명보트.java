fde6c7aa66b3c113dfa197d57107e2263e4cf802
sksk713 <sskk713@ajou.ac.kr>
2021-07-24 03:12:09 +0900

    update

--- Solve007/구명보트.java
+++ Solve007/구명보트.java
@@ -3,27 +3,34 @@ package Solve007;
 import java.util.*;
 
 class 구명보트 {
-    public int solution(int[] people, int limit) {
+    public static void main(String[] args) {
+        int[] a = {70, 80, 50};
+        int limit = 100;
+        System.out.println(solution(a, limit));
+    }
+    static int solution(int[] people, int limit) {
         boolean[] visit = new boolean[people.length];
         int cnt = 0;
         int x = limit;
 
         Arrays.sort(people);
         int flag = 0;
+        int j = people.length - 1;
 
         for (int i = 0; i < people.length; i++) {
             if (visit[i]) {
                 continue;
             }
+            if (limit - people[i] < people[i]) {
+                visit[i] = true;
+                cnt++;
+                continue;
+            }
             limit = x;
             flag = 0;
-            for (int j = people.length - 1; j > i; j--) {
+
+            for (; j > i; j--) {
                 if (visit[j]) {
-                    continue;
-                }
-                if (limit - people[j] < 40) {
-                    visit[j] = true;
-                    cnt++;
                     continue;
                 }
                 if (limit >= (people[i] + people[j])) {
@@ -33,6 +40,8 @@ class 구명보트 {
                     cnt++;
                     break;
                 }
+                visit[j] = true;
+                cnt++;
             }
             if (flag != 1) {
                 visit[i] = true;
